e43a480813e2cd88d1cac26e43ea70e843960184
_Justrishi <rishimud@gmail.com>
2021-07-28 22:04:13 +0530

    Lobby setup
    Scenes are changeable, buttons are working, full fledged game is created

--- Assets/Scripts/LevelOverControl.cs
+++ Assets/Scripts/LevelOverControl.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelOverControl : MonoBehaviour
 
 {
+    public int Level;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
         if (collision.gameObject.GetComponent<PlayerController>())
         {
-            Debug.Log("Level Complete");
+            //Debug.Log("Level Complete");
+            SceneManager.LoadScene(Level);
         }
     }
 }
